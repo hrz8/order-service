cfc4804c5fdf28b7d6ff6c4b4ff8d11b4634f031
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2022-11-19 22:30:36 +0700

    fix

--- terraform-services.tf
+++ terraform-services.tf
@@ -108,7 +108,8 @@ resource "google_api_gateway_api_config" "api_cfg_0" {
     google_project_service.apigateway,
     google_project_service.servicemanagement,
     google_project_service.servicecontrol,
-    google_cloud_run_service.service
+    google_cloud_run_service.service,
+    google_api_gateway_gateway.api_gw
   ]
 }
 
@@ -116,10 +117,4 @@ resource "google_api_gateway_gateway" "api_gw" {
   provider   = google-beta
   api_config = google_api_gateway_api_config.api_cfg_0.id
   gateway_id = format("%s-api-gw", local.service_alias)
-
-  lifecycle {
-    replace_triggered_by = [
-      google_api_gateway_api_config.api_cfg_0.id
-    ]
-  }
 }
